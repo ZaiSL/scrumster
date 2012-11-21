3e6ebae1e7a0ccb4493a580c4c88e730929ecfed
pavel <pavel@nyshka.(none)>
2012-11-21 18:47:15 +0600

    Добавление задачи без излишних валидаций

--- app/controllers/ApiController.php
+++ app/controllers/ApiController.php
@@ -9,6 +9,8 @@ class ApiController extends ControllerBase {
 		
 		$issues = array();
 		foreach(Issues::find() as $issue) {
+			$issues[] = $issue;
+			/*
 			$issues[] = array(
 				'id' => $issue->id,
 				'project_id' => $issue->project_id,
@@ -24,11 +26,12 @@ class ApiController extends ControllerBase {
 				'parent_feature_id' => $issue->parent_feature_id,
 				'created_at' => $issue->created_at,
 			);
+			*/
 		}
 		
-        $this->view->setVar('success',true);
-        $this->view->setVar('message','ok');
-        $this->view->setVar('items',$issue);
+        $this->view->setVar('success', true);
+        $this->view->setVar('message', 'ok');
+        $this->view->setVar('items', $issues);
        
 	}
 	
@@ -36,17 +39,25 @@ class ApiController extends ControllerBase {
 	 * Добавление ишью
 	 */
 	public function issueAddAction() {
-        
+
+		// Для теста метода можно использовать:
+		// curl -i -X POST -d '{"project_id":1,"sprint_id":1,"type":"task","title":"Ой-ой, первый таск","description":"Пивасик, ну","estimate":2,"is_feature":0,"stage":1,"owner_id":1,"assigned_to":0,"parent_feature_id":"1","created_at":0}' http://www.scrumster.local/api/issues/
+
 		$request = new \Phalcon\Http\Request();
 
 		$issue_data = json_decode($request->getRawBody(), true);
 
 		$issue = \Phalcon\Mvc\Model::dumpResult(new Issues(), $issue_data);
-		$issue->save();
 
-        $this->view->setVar('success',true);
-        $this->view->setVar('message','ok');
-        $this->view->setVar('data',$issue);
+		if ($issue->save()) {
+			$this->view->setVar('success', true);
+			$this->view->setVar('message', 'ok');
+			$this->view->setVar('data', $issue);
+		} else {
+			$this->view->setVar('success', false);
+			$this->view->setVar('message', 'Ошибка добавления задачи');
+		}
+
 	}
 	
 	/**
